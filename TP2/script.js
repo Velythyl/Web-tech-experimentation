ae9115822b9a56299666732548b128a15dad6880
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-03-09 15:37:40 -0500

    Add files via upload

--- TP2/script.js
+++ TP2/script.js
@@ -1,19 +1,21 @@
 var grid;
-var gridDiv;
+let gap = 4; //4 px de gap
 const duration = 300;
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#alert").hide();
 
-    gridDiv = $("#grid");
-
-    $("#init-button").click(function() {
-       initGame();
+    $("#init-button").click(function () {
+        initGame();
     });
 
     $("#return-button").click(function () {
         $("#init-wrapper").show();
-    })
+    });
+
+    $(window).resize(function () {
+        buildGrid();
+    });
 });
 
 function initGame() {
@@ -23,7 +25,7 @@ function initGame() {
     let x = parseInt(xStr);
     let y = parseInt(yStr);
 
-    if(xStr === "" || yStr === "" || x < 2 || y < 2) {
+    if (xStr === "" || yStr === "" || x < 2 || y < 2) {
         $("#alert").show();
         return;
     }
@@ -37,82 +39,90 @@ function initGame() {
     $("#lost").show();
     $("#won").show();
     $("#alert").hide();
+
+    setKeyEvents();
 }
 
-$(document).keyup(function(e) {
-    if(!grid.ready) return; //on doit attendre que la grille se soit fait updater avant de jouer...
+function setKeyEvents() {
+    $(document).one("keyup", function (e) {
+        switch (e.which) {
+            case 65:    //a
+            case 37:    //left
+                grid.move(1);
+                break;
 
-    switch(e.which) {
-        case 65:    //a
-        case 37:    //left
-            grid.move(1);
-            break;
+            case 87:    //w
+            case 38:    //up
+                grid.move(2);
+                break;
 
-        case 87:    //w
-        case 38:    //up
-            grid.move(2);
-            break;
+            case 68:    //d
+            case 39:    //right
+                grid.move(-1);
+                break;
 
-        case 68:    //d
-        case 39:    //right
-            grid.move(-1);
-            break;
+            case 83:    //s
+            case 40:    //down
+                grid.move(-2);
+                break;
 
-        case 83:    //s
-        case 40:    //down
-            grid.move(-2);
-            break;
+            case 13:    //enter
+                if ($("#init-wrapper").is(":visible")) initGame();
+            default:
+                return;
+        }
 
-        case 13:    //enter
-            if($("#init-wrapper").is(":visible")) initGame();
-            return;
-
-        default:
-            return;
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-});
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+}
 
 function endTurn() {
     let xy = grid.genRand();
     update();
 
-    if(xy !== false) $("#x"+xy[0]+"y"+xy[1]).css({"animation-name": "rotate", "animation-duration": ""+duration+"ms", "animation-timing-function": "linear"});
-    else checkForLoss();    //si on a pas trouve de cellules null dans genrand, on a une possibilite d'avoir perdu: on le teste
+    if (xy !== false) $("#x" + xy[0] + "y" + xy[1]).css({
+        "animation-name": "rotate",
+        "animation-duration": "" + duration + "ms",
+        "animation-timing-function": "linear"
+    });
 
-    if(grid.won || grid.lost) {
+    checkForLoss();    //si on a pas trouve de cellules null dans genrand, on a une possibilite d'avoir perdu: on le teste
+
+    if (grid.won || grid.lost) {
         $("#game").hide();
 
-        if(grid.lost) $("#won").hide();
-        if(grid.won) $("#lost").hide();
+        if (grid.lost) $("#won").hide();
+        if (grid.won) $("#lost").hide();
     }
-
-    grid.ready = true;
 }
 
 function checkForLoss() {
-    for(let i=0; i<grid.x; i++) {
-        for(let j=0; j<grid.y; j++) {
+    for (let i = 0; i < grid.x; i++) {
+        for (let j = 0; j < grid.y; j++) {
             let val = grid.grid[i][j].value;
+
+            if(val === null) return;
 
             /*
                 Les try-catch permettent de beaucoup simplifier la logique: on ne fait que betement prendre tous les
                 voisins de la cellule, et tant pis si c'est hors de la grille.
              */
             try {
-                const up = grid.grid[i][j-1].value;
-                if(up === val) return;
-            } catch (ignored) {}
-            try {
-                const down = grid.grid[i][j+1].value;
-                if(down === val) return;
-            } catch (ignored) {}
-            try {
-                const left = grid.grid[i-1][j].value;
-                if(left === val) return;
+                const up = grid.grid[i][j - 1].value;
+                if (up === val) return;
             } catch (ignored) {
-                const right = grid.grid[i+1][j].value;
-                if(right === val) return;
+            }
+            try {
+                const down = grid.grid[i][j + 1].value;
+                if (down === val) return;
+            } catch (ignored) {
+            }
+            try {
+                const left = grid.grid[i - 1][j].value;
+                if (left === val) return;
+            } catch (ignored) {
+                const right = grid.grid[i + 1][j].value;
+                if (right === val) return;
             }
         }
     }
@@ -131,173 +141,184 @@ function newGame(x, y) {
     grid.y = y;
     grid.grid = [];
 
-    grid.stateChanges = {};
-
-    for(let i=0; i<x; i++) {
-        var tempArr = [];
-        for(let j=0; j<y; j++) {
+    for (let i = 0; i < x; i++) {
+        let tempArr = [];
+        for (let j = 0; j < y; j++) {
             tempArr.push(newTile(i, j));
         }
 
         grid.grid.push(tempArr);
     }
 
-    grid.genRand = function() {
-        var nullCells = [];
+    grid.genRand = function () {
+        let nullCells = [];
 
-        for(let i=0; i<x; i++) {
-            for(let j=0; j<y; j++) {
-                if(this.grid[i][j].value === null) nullCells.push([i, j]);
+        for (let i = 0; i < x; i++) {
+            for (let j = 0; j < y; j++) {
+                if (this.grid[i][j].value === null) nullCells.push([i, j]);
             }
         }
 
-        if(nullCells.length === 0) return false;
+        if (nullCells.length === 0) return false;
 
         const choice = nullCells[rand(nullCells.length)];
 
         const i = choice[0];
         const j = choice[1];
 
-        this.grid[i][j].value = (rand(2)+1)*2;
-        this.stateChanges["x"+i+"y"+j] = [i, j];
+        this.grid[i][j].value = (rand(2) + 1) * 2;
+        this.stateChanges["x" + i + "y" + j] = [i, j];
 
         return [i, j];
     };
 
-    grid.ready = true;  //le prochain tour est il pret?
-    grid.move = function(dir) {
-        this.nb++;
-        this.stateChanges = {};
-        this.ready = false;
+    grid.stateChanges = {};
+    grid.lastMoved = null;
+    /*
+        Prends une direction et la rend abstraite a l'aide des sous-fonctions de direction (left, right, up, down).
 
-        let last;   //last tile to be changed
+        Puis, appelle makeMove, qui fait un mouvement abstrait et les animations correspondantes
+     */
+    grid.move = function (dir) {
+        grid.lastMoved = null;
 
-        const gridSpacer = " + 0.25em * ";
-
-        switch (dir) {
-            case -1:
-            case 1: //left
-                for(let j=0; j<this.y; j++) {
-                    for(let falseI=1; falseI<this.x; falseI++) {
-
-                        let i = (dir === 1? falseI : this.x-1-falseI);
-
-                        let x = i;
-                        let y = j;
-
-                        while(true) {
-                            let cell = this.grid[x][y];
-                            let nextCell = this.grid[x-dir][y];
-
-                            let cond = cell.moveInto(nextCell, i, j);
-                            if(cond === 0) break;
-
-                            x -= dir;
-
-                            if(cond === 1) break;
-
-                            if(x-dir<0 || x-dir >= this.x) break;
-                        }
-
-                        if(x !== i) {
-                            x = (x-i);
-
-                            last = $("#x"+i+"y"+j);
-                            last.css({"transition": "transform "+duration+"ms", "transform": ("translateX( calc( 100% * " + x + gridSpacer + x + " ) )") });
-                        }
-                    }
-                }
-                break;
-            case -2: //down
-            case 2: //up
-                dir /= 2;
-
-                for(let i=0; i<this.x; i++) {
-                    for(let falseJ=1; falseJ<this.y; falseJ++) {
-
-                        let j = (dir === 1? falseJ : this.y-1-falseJ);
-
-                        let x = i;
-                        let y = j;
-
-                        while(true) {
-                            let cell = this.grid[x][y];
-                            let nextCell = this.grid[x][y-dir];
-
-                            let cond = cell.moveInto(nextCell, i, j);
-                            if(cond === 0) break;
-
-                            y -= dir;
-
-                            if(cond === 1) break;
-
-                            if(y-dir<0 || y-dir>=this.y) break;
-                        }
-
-                        if(y !== j) {
-                            y = (y-j);
-
-                            last = $("#x"+i+"y"+j);
-                            last.css({"transition": "transform "+duration+"ms", "transform": ("translateY( calc( 100% * " + y + gridSpacer + y + " ) )") });
-                        }
-                    }
-                }
-                break;
+        function left(x, y) {
+            return ok(x - 1, y);
         }
 
-        //https://blog.teamtreehouse.com/using-jquery-to-detect-when-css3-animations-and-transitions-end
-        let hasNotFired = true;
-        last.bind( 'transitionend', function() {
-            if(hasNotFired) endTurn();
-            hasNotFired = false;   //previent de fire le event deux fois
-        });
+        function right(x, y) {
+            return ok(x + 1, y);
+        }
+
+        function up(x, y) {
+            return ok(x, y - 1);
+        }
+
+        function down(x, y) {
+            return ok(x, y + 1);
+        }
+
+        function ok(x, y) {
+            if (y < 0 || y >= grid.y || x < 0 || x >= grid.x) return null;
+            else return grid.grid[x][y];
+        }
+
+        this.nb++;
+        this.stateChanges = {};
+
+        if (dir === -1 || dir === 1) {
+            for (let j = 0; j < this.y; j++) {
+                for (let falseI = 1; falseI < this.x; falseI++) {
+
+                    let i = (dir === 1 ? falseI : this.x - 1 - falseI);
+                    this.makeMove(i, j, dir === -1 ? right : left);
+                }
+            }
+        } else {
+            dir /= 2;
+            for (let i = 0; i < this.x; i++) {
+                for (let falseJ = 1; falseJ < this.y; falseJ++) {
+
+                    let j = (dir === 1 ? falseJ : this.y - 1 - falseJ);
+                    this.makeMove(i, j, dir === -1 ? down : up);
+                }
+            }
+        }
+
+        if(this.lastMoved !== null) {
+            //https://blog.teamtreehouse.com/using-jquery-to-detect-when-css3-animations-and-transitions-end
+            this.lastMoved.bind('transitionend', function () {
+                endTurn();
+                grid.lastMoved.unbind();
+                setKeyEvents(); //refait les key events pour le prochain move
+            });
+        } else {
+            setKeyEvents();
+        }
+    };
+    /*
+        Fait un mouvement abstrait decrit par dir a l'aide de moveValue, fonction qui bouge une valeur vers une
+        coordonnee donnee par dir.
+     */
+    grid.makeMove = function (i, j, dir) {
+        function moveValue(value, x, y) {
+            if (value === null) return false;
+
+            let tile = dir(x, y);
+
+            if (tile === null) return false;
+
+            //let temp = grid.stateChanges["x"+tile.x+"y"+tile.y];
+
+            if (value === tile.value) {
+
+                /*
+                    regarder si la tile a ete barree (etrangement juste aller voir dans l'objet tile ne fonctionne pas,
+                    bug de JS?)
+                 */
+                try {
+                    if(grid.stateChanges["x"+tile.x+"y"+tile.y][2]) return false;
+                } catch (ignored) {}
+
+                tile.value = tile.value * 2;
+                tile.locked = true;
+
+                if (tile.value === 2048) grid.won = true;
+
+                return tile;
+            } else if (tile.value === null) {
+                let result = moveValue(value, tile.x, tile.y, dir);
+                if (result === false) {
+                    tile.value = value;
+                    result = tile;
+                }
+
+                return result;
+            }
+
+            return false;
+        }
+
+        const gridSpacer = " + " + gap + "px * ";
+
+        const editedTile = moveValue(this.grid[i][j].value, i, j);
+        if (editedTile !== false) {
+            this.stateChanges["x" + editedTile.x + "y" + editedTile.y] = [editedTile.x, editedTile.y, editedTile.locked];
+
+            editedTile.locked = false;  //reset les locks
+
+            this.grid[i][j].value = null;
+            this.stateChanges["x" + i + "y" + j] = [i, j, false];
+
+            let x = (editedTile.x - i);
+            let y = (editedTile.y - j);
+
+            let last = $("#x" + i + "y" + j);
+            last.css({
+                "transition": "transform " + duration + "ms",
+                "transform": "translate( calc( 100% * " + x + gridSpacer + x + " ), calc( 100% * " + y + gridSpacer + y + " ) )"
+            });
+
+            this.lastMoved = last;
+        }
     };
 
-    grid.genRand();
+    grid.genRand(); //on met deux nouvelles tuiles dans la grille
     grid.genRand();
 
-    buildGrid();
+    buildGrid();    //on construit la grille avec les bonnes valeurs
 }
 
+/*
+    Voir une tile comme un datatype, pas comme un objet...
+ */
 function newTile(x, y) {
     let obj = {};
 
     obj.value = null;
     obj.x = x;
     obj.y = y;
-
-    /*
-        Retourne 0 pour aucun mouvements,
-        1 pour s'il y a eu un mouvement et qu'il ne peut y en avoir d'autres
-        2 s'il y a eu un mouvement et qu'il peut y en avoir d'autres
-     */
-    obj.moveInto = function (tile, i, j) {
-        if(this.value === null) return 0;
-
-        if(this.value === tile.value) {
-            this.value = null;
-            tile.value = tile.value*2;
-            if(tile.value === 2048) grid.won = true;
-
-            this.changeDict(tile, i, j);
-
-            return 1;
-        } else if(tile.value === null) {
-            tile.value = this.value;
-            this.value = null;
-
-            this.changeDict(tile, i, j);
-
-            return 2;
-        }
-
-        return 0;
-    };
-
-    obj.changeDict = function (tile, i, j) {
-        grid.stateChanges["x"+i+"y"+j] = [i, j];
-        grid.stateChanges["x"+tile.x+"y"+tile.y] = [tile.x, tile.y];
-    };
+    obj.locked = false;
 
     return obj;
 }
@@ -307,52 +328,53 @@ function rand(max) {
 }
 
 function buildGrid() {
-    let sheet = document.styleSheets[0];
     let inside = "";
+    let css = "";
 
-    let gap = 4; //4 px de gap
+    let gridDiv = $("#grid");
 
     //cote de grid / nb de carres - les gaps entre les carres
-    let width = gridDiv.width()/grid.x - gap;
-    let height = gridDiv.height()/grid.y - gap;
+    let width = gridDiv.width() / grid.x - gap;
+    let height = gridDiv.height() / grid.y - gap;
 
-    for(let i=0; i<grid.x; i++) {
-        for(let j=0; j<grid.y; j++) {
+    for (let i = 0; i < grid.x; i++) {
+        for (let j = 0; j < grid.y; j++) {
             let val = grid.grid[i][j].value;
 
-            let key = 'x'+i+'y'+j;
-
-            if(val !== null) inside += '<div class="tile v'+val+'" id="'+key+'"><div class="v">' + val + '</div></div>';
-            else inside += '<div class="tile" id="'+key+'"><div class="v"></div></div>';
-
             /*
-                Pourrait facilement etre fait avec un css grid (donc, donner a grid: display: grid; grid-gap 4px;
+                Ceci pourrait facilement etre fait avec un css grid (donc, donner a grid: display: grid; grid-gap 4px;
                 grid-auto-flow: column; et le nombre approprie de rows pour grid.x),
                 mais ceci n'est pas compatible avec position: absolute ou fixed pour les grid items et on en a
                 absolument besoin pour eviter des tonnes de reflow lors des animations du tour.
+
+                De plus, il serait plus elegant de placer les regles de css de position et de grosseur dans une feuille
+                css externe, mais en faisant ceci on ne peut plus changer la grosseur de la fenetre du browser sans
+                faire une manipulation compliquee des regles de css.
              */
-            sheet.insertRule("#"+key+" { left: "+(width + gap)*i+"px; top: "+(height + gap)*j+"px; width: "+width+"px; height: "+height+"px; }");
+
+            let key = 'x' + i + 'y' + j;
+
+            css += "#" + key + "{ left: " + (width + gap + gap / (grid.x - 1)) * i + "px; top: " + (height + gap + gap / (grid.y - 1)) * j + "px; width: " + width + "px; height: " + height + "px; }\n";
+
+            inside += '<div class="tile v' + val + '" id="' + key + '"><div class="v">' + (val !== null ? val : "") + '</div></div>';
         }
     }
 
+    $("style").html(css);   //efface l'ancien css et le remplace par le nouveau
     gridDiv.html(inside);
 }
 
 function update() {
-    $(".counter").text(""+grid.nb);
+    $(".counter").text("" + grid.nb);
 
-    gridDiv.hide(); //prevent reflow
-
-    for(let key in grid.stateChanges) {
-        const tile = $("#"+key);
+    for (let key in grid.stateChanges) {
+        const tile = $("#" + key);
 
         const arr = grid.stateChanges[key];
         const val = grid.grid[arr[0]][arr[1]].value;
 
         tile.attr("style", "");
-        tile.attr("class", "tile "+(val === null ? "" : "v"+val));
-        tile.html('<div class="v">' + (val === null ? "" : ""+val) + '</div>');
+        tile.attr("class", "tile " + "v" + val);
+        tile.html('<div class="v">' + (val === null ? "" : "" + val) + '</div>');
     }
-
-    gridDiv.show();
 }
