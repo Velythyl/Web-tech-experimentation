07170ba1aedf1261b2098f290f9fe0900ebde3a4
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-04-04 14:06:57 -0400

    Add files via upload

--- TP3/Generateur/init.sh
+++ TP3/Generateur/init.sh
@@ -2,6 +2,6 @@
 
 echo "You provided argument $1"
 
-mysql -u gautchar -p -h mysql -e <DDL.sql
+mysql -u gautchar -p -h mysql <DDL.sql
 mysql -u gautchar -p -h mysql gautchar_IFT3225TP3 <Populate.sql
-mysql -u gautchar -p -h mysql -e <DML.sql
+mysql -u gautchar -p -h mysql gautchar_IFT3225TP3 <DML.sql
