424eb570e46b9c8635534e330f0df11f3d3663a9
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-04-11 21:17:37 -0400

    Add files via upload

--- TP3/Generateur/DML.sql
+++ TP3/Generateur/DML.sql
@@ -26,6 +26,24 @@ RETURN
 );
 
 DELIMITER $$
+CREATE FUNCTION used_by (field INTEGER, day DATE, hour INTEGER, player_id INTEGER)
+RETURNS VARCHAR(10)
+BEGIN
+	DECLARE player VARCHAR(10);
+	SET player = (SELECT J_ID FROM Reservation WHERE T_ID=field AND R_date=day AND Heure=hour);
+
+	IF player IS NULL THEN
+		RETURN "Free";
+	ELSEIF player=player_id THEN
+		RETURN "You";
+	ELSE
+		RETURN "Taken";
+	END IF;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE FUNCTION create_user(name VARCHAR(255), fname VARCHAR(255), mdp VARCHAR(30), uname VARCHAR(30))
 RETURNS INT
 BEGIN
@@ -38,12 +56,25 @@ END;
 $$
 DELIMITER ;
 
-
-
-/*Insert a new expert/categories pair to the expert table*/
-CREATE OR REPLACE FUNCTION del_expertise(id_expert INT, cat VARCHAR)
-RETURNS void AS $$
+DELIMITER $$
+CREATE PROCEDURE all_on_day(day DATE, hour VARCHAR(2), field VARCHAR(1), player INTEGER)
 BEGIN
-	PERFORM DELETE FROM expert WHERE nom_categorie=cat;
-END; $$
-LANGUAGE 'plpgsql';
+	CREATE TEMPORARY TABLE hourTable (h INTEGER);
+	IF hour='a' THEN
+		INSERT INTO hourTable VALUES(6), (7), (8), (9), (10), (11), (12), (13), (14), (15), (16), (17), (18), (19), (20);
+	ELSE
+		INSERT INTO hourTable VALUES(CAST(hour AS UNSIGNED)); 
+	END IF;
+
+	CREATE TEMPORARY TABLE fieldTable (f INTEGER);
+	IF field='a' THEN
+		INSERT INTO fieldTable VALUES(1), (2), (3), (4), (5);
+	ELSE
+		INSERT INTO fieldTable VALUES(CAST(field AS UNSIGNED)); 
+	END IF;
+
+	SELECT f, h, used_by(f, day, h, player) FROM (SELECT * FROM fieldTable CROSS JOIN hourTable) AS temp;
+		
+END;
+$$
+DELIMITER ;
